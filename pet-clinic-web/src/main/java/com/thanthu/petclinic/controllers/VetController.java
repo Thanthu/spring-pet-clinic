45628c7ae4715a7d6dec2b5fa984eab38b7960c2
Thanthu <tnair@orthofx.com>
2021-08-01 22:42:12 +0530

    List all Vets on Vet index page. Closes #14.

--- pet-clinic-web/src/main/java/com/thanthu/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/thanthu/petclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.thanthu.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.thanthu.petclinic.model.services.VetService;
 
 @Controller
 public class VetController {
+	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
 
 	@RequestMapping("/vets")
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 
