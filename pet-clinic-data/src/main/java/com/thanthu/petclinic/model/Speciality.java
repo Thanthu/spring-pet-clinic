ed259ba585cd70e74501cbada90a08155f2a4ea4
Thanthu <tnair@orthofx.com>
2021-08-08 07:33:54 +0530

    Create Visit Entity. Closes #38.

--- pet-clinic-data/src/main/java/com/thanthu/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/thanthu/petclinic/model/Speciality.java
@@ -1,5 +1,10 @@
 package com.thanthu.petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sepcialities")
 public class Speciality extends BaseEntity {
 	
 	private String description;
--- pet-clinic-data/src/main/java/com/thanthu/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/thanthu/petclinic/model/Vet.java
@@ -3,8 +3,19 @@ package com.thanthu.petclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
-	
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialitites", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<Speciality> specialities = new HashSet<Speciality>();
 
 	public Set<Speciality> getSpecialities() {
@@ -14,5 +25,5 @@ public class Vet extends Person {
 	public void setSpecialities(Set<Speciality> specialities) {
 		this.specialities = specialities;
 	}
-	
+
 }
