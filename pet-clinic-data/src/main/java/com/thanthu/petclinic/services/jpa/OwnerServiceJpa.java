b15512950bd32897aa687641bddfa19a425f375d
Thanthu <tnair@orthofx.com>
2021-08-08 08:39:31 +0530

    Create Spring Data JPA Service for Owners. Closes #40.

---
+++ pet-clinic-data/src/main/java/com/thanthu/petclinic/services/jpa/OwnerServiceJpa.java
@@ -0,0 +1,55 @@
+package com.thanthu.petclinic.services.jpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.thanthu.petclinic.model.Owner;
+import com.thanthu.petclinic.repositories.OwnerRepository;
+import com.thanthu.petclinic.services.OwnerService;
+
+@Service
+@Profile("jpa")
+public class OwnerServiceJpa implements OwnerService {
+
+	private final OwnerRepository ownerRepository;
+
+	public OwnerServiceJpa(OwnerRepository ownerRepository) {
+		this.ownerRepository = ownerRepository;
+	}
+
+	@Override
+	public Set<Owner> findAll() {
+		Set<Owner> owners = new HashSet<>();
+		ownerRepository.findAll().forEach(owners::add);
+		return owners;
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return ownerRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		return ownerRepository.save(object);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		ownerRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		ownerRepository.deleteById(id);
+	}
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		return ownerRepository.findByLastName(lastName);
+	}
+
+}
