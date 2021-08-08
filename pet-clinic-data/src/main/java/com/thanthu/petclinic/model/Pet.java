75faf4a4c0327a0533cfe38a61e46d793fd44070
Thanthu <tnair@orthofx.com>
2021-08-08 08:10:11 +0530

    Create Visit Entity. Closes #37.

--- pet-clinic-data/src/main/java/com/thanthu/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/thanthu/petclinic/model/Pet.java
@@ -1,44 +1,62 @@
 package com.thanthu.petclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "pets")
 public class Pet extends NamedEntity {
-	
+
 	private LocalDate birthDate;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "type_id")
 	private PetType petType;
-	
+
 	@ManyToOne
 	private Owner owner;
-	
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<Visit>();
+
 	public LocalDate getBirthDate() {
 		return birthDate;
 	}
+
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
+
 	public PetType getPetType() {
 		return petType;
 	}
+
 	public void setPetType(PetType petType) {
 		this.petType = petType;
 	}
+
 	public Owner getOwner() {
 		return owner;
 	}
+
 	public void setOwner(Owner owner) {
 		this.owner = owner;
 	}
-	
-	
+
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
+	}
 
 }
