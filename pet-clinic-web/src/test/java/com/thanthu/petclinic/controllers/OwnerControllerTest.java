ab93e5ebd488721ecf143838456deea8eeb15562
Thanthu <tnair@orthofx.com>
2021-08-15 08:52:46 +0530

    Add ability to add or update an Owner. Closes #55.

--- pet-clinic-web/src/test/java/com/thanthu/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/thanthu/petclinic/controllers/OwnerControllerTest.java
@@ -4,8 +4,14 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -17,6 +23,7 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -52,7 +59,7 @@ class OwnerControllerTest {
 		mockMvc.perform(get("/owners/find")).andExpect(status().isOk()).andExpect(view().name("owners/findOwners"))
 				.andExpect(model().attributeExists("owner"));
 
-		verifyNoInteractions(ownerService);
+		verifyNoMoreInteractions(ownerService);
 	}
 
 	@Test
@@ -78,6 +85,44 @@ class OwnerControllerTest {
 
 		mockMvc.perform(get("/owners/123")).andExpect(status().isOk()).andExpect(view().name("owners/ownerDetails"))
 				.andExpect(model().attribute("owner", hasProperty("id", is(1l))));
+	}
+
+	@Test
+	void initCreationForm() throws Exception {
+		mockMvc.perform(get("/owners/new")).andExpect(status().isOk())
+				.andExpect(view().name("owners/createOrUpdateOwnerForm")).andExpect(model().attributeExists("owner"));
+
+		verifyNoMoreInteractions(ownerService);
+	}
+
+	@Test
+	void processCreationForm() throws Exception {
+		when(ownerService.save(ArgumentMatchers.any())).thenReturn(Owner.builder().id(1L).build());
+
+		mockMvc.perform(post("/owners/new")).andExpect(status().is3xxRedirection())
+				.andExpect(view().name("redirect:/owners/1")).andExpect(model().attributeExists("owner"));
+
+		verify(ownerService).save(ArgumentMatchers.any());
+	}
+
+	@Test
+	void initUpdateOwnerForm() throws Exception {
+		when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1l).build());
+
+		mockMvc.perform(get("/owners/1/edit")).andExpect(status().isOk())
+				.andExpect(view().name("owners/createOrUpdateOwnerForm")).andExpect(model().attributeExists("owner"));
+
+		verifyNoMoreInteractions(ownerService);
+	}
+
+	@Test
+	void processUpdateOwnerForm() throws Exception {
+		when(ownerService.save(ArgumentMatchers.any())).thenReturn(Owner.builder().id(1L).build());
+
+		mockMvc.perform(post("/owners/1/edit")).andExpect(status().is3xxRedirection())
+				.andExpect(view().name("redirect:/owners/1")).andExpect(model().attributeExists("owner"));
+
+		verify(ownerService).save(ArgumentMatchers.any());
 	}
 
 }
