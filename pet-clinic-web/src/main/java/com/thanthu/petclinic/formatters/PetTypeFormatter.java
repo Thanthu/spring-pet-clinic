bdf9ae7b85bcdfe794b34329948ae788a922925f
Thanthu <tnair@orthofx.com>
2021-08-15 11:40:40 +0530

    Add ability to add or update a Pet. Closes #56.

---
+++ pet-clinic-web/src/main/java/com/thanthu/petclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,40 @@
+package com.thanthu.petclinic.formatters;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import com.thanthu.petclinic.model.PetType;
+import com.thanthu.petclinic.services.PetTypeService;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+	private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+
+}
